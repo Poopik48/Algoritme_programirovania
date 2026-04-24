2d17349d79dd65adae2e4d1b095544c4d4756844
Poopik48 <shahruebv3101@gmail.com>
2026-04-24 09:26:16 +0300

    Delete 4.5.py

--- 4.5.py
+++
@@ -1,9 +0,0 @@
-data = [float(x) for x in input("Введите числа через пробел: ").split()]
-
-s = 0
-
-for i, x in enumerate(data, start=1):
-    if x < 0:
-        s += i
-
-print(f"Сумма индексов отрицательных чисел: {s}")
